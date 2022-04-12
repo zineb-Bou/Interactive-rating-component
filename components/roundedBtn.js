9f7255286b1b4a35afe79616600cdcd5d71114a4
unknown <boutaazineb08@gmail.com>
2022-04-12 12:18:15 +0100

    Correct typos.

--- components/roundedBtn.js
+++ components/roundedBtn.js
@@ -3,6 +3,7 @@ export default function RoundedBtn({ children, onClick }) {
     <button
       className="bg-DarkBlue text-MediumGrey rounded-full border-none px-4 py-2 cursor-pointer hover:bg-LightGrey hover:text-White focus:bg-Orange focus:text-White"
       onClick={onClick}
+      aria-label={`${children} star`}
     >
       {children}
     </button>
